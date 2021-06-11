12dbb98895152a316052ea40ea702c90c174bce1
Helena0607 <83812793+Helena0607@users.noreply.github.com>
2021-06-11 16:59:26 +0700

    Update if.js

--- javascript/if.js
+++ javascript/if.js
@@ -3,9 +3,11 @@ let shopAddress = prompt(`Welcome to ZeeKwek Eyewear!`, '');
 if (shopAddress == 'Tamwe') {
     alert('No.325, Kyaikasan Road, Tamwe, Yangon');
 } else if (shopAddress == 'Thingangyun') {
-    alert('33 Lay Daung Kan Road, Thingangyun, Yangon')
+    alert('33 Lay Daung Kan Road, Thingangyun, Yangon');
 } else if (shopAddress == 'Myaynigone') {
-    alert('No.267, Ground Floor, Myaynigone, Yangon')
-} else {
-    alert('No.34, Yangon Insein Road, Insein, Yangon')
+    alert('No.267, Ground Floor, Myaynigone, Yangon');
+} else if(shopAddress == 'Insein'){
+    alert('No.34, Yangon Insein Road, Insein, Yangon');
+} else{
+    alert('Please write your address.');
 }
